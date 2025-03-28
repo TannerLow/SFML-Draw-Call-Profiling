5b8df827b2d94be899b22b7d81f57c015aee1ad8
Tanner Lowthorp <lowthorpt@gmail.com>
2025-03-27 21:04:20 -0500

    display is the problem

--- SFML-Draw-Call-Profiling/src/main.cpp
+++ SFML-Draw-Call-Profiling/src/main.cpp
@@ -8,13 +8,14 @@
 int main() {
     sf::RenderWindow window(sf::VideoMode({ 1600, 900 }), "SFML Draw Call Profiling");
 
-    //SpriteRenderer renderer(5000);
+    //SpriteRenderer renderer(1000);
     DynamicVertexArrayRenderer renderer(10000);
 
     sf::Clock drawClock;
-    std::vector<int64_t> drawTimes;
     int frameCount = 0;
-    int maxFrameCount = 5000;
+    int maxFrameCount = 2000;
+    int64_t drawTimeSum = 0;
+
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -26,27 +27,35 @@ int main() {
         renderer.update();
 
 
-        drawClock.restart();
+        //drawClock.restart();
 
         window.clear();
+
+        ///////////////
+        //drawClock.restart();
+
+        //window.clear();
         window.draw(renderer);
+
+        drawClock.restart();
         window.display();
 
         int64_t drawTime = drawClock.getElapsedTime().asMicroseconds();
-        drawTimes.push_back(drawTime);
+        drawTimeSum += drawTime;
         frameCount++;
 
-        if (frameCount == maxFrameCount) {
+        if (frameCount >= maxFrameCount) {
             frameCount = 0;
-            int64_t sum = 0;
-            for (auto time : drawTimes) {
-                sum += time;
-            }
-            double avg = (double)sum / maxFrameCount;
+            double avg = (double)drawTimeSum / maxFrameCount;
             double fps = 1000000.0 / avg;
             printf("Average us per frames across %d frames: %.2f = ", maxFrameCount, avg);
             printf("%.2f fps\n", fps);
-            drawTimes.clear();
+            drawTimeSum = 0;
         }
+        ///////////////
+
+        //window.display();
     }
+
+    return 0;
 }
